dca337a927d7892ec5dfb2546e87b56d27a27d0e
Willyan Fernando <Wff.cirino@gmail.com>
2022-07-18 09:35:12 -0300

    Ajuste para gateway de beneficiario api

--- apigateway/src/main/java/com/cn/novaera/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/cn/novaera/apigateway/ApigatewayApplication.java
@@ -2,12 +2,26 @@ package com.cn.novaera.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+					.route(r -> r.path("/api-beneficiario/**").uri("lb://cnbeneficiario"))
+					.build();
+	}
 }
